e4550b007bfd86b20eea0da579cf5ac51b9709d3
ivan-comar <guardian.sanddy@gmail.com>
2017-12-11 00:29:02 +0800

    update app file

--- app.js
+++ app.js
@@ -6,13 +6,19 @@ var budgetController = (function() {
 // UI Controller
 var UIController = (function() {
 
-})
+})()
 
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
+    var ctrilAddItem = function() {
+        // some codes
+        console.log('ENTER was pressed!')
+    }
+    document.querySelector('.add__btn').addEventListener('click', ctrilAddItem)
+
     document.addEventListener('keypress', function(event) {
         if (event.keyCode === 13 || event.which === 13) {
-            console.log('ENTER was pressed!')
+            ctrilAddItem()
         }
     })
 })(budgetController, UIController)
