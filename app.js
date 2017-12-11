e1436d0c2c9de69359f920751525cbebcc0910dd
ivan-comar <guardian.sanddy@gmail.com>
2017-12-11 14:52:49 +0800

    update app file

--- app.js
+++ app.js
@@ -5,29 +5,51 @@ var budgetController = (function() {
 
 // UI Controller
 var UIController = (function() {
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
     return {
         getInput: function() {
             return {
-                type: document.querySelector('.add__type').value,
-                description: document.querySelector('.add__description').value,
-                value: document.querySelector('.add__value').value
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
             }
+        },
+        getDOMstrings: function() {
+            return DOMstrings
         }
     }
 })()
 
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings()
+        // click the add item
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrilAddItem)
+        // press the enter 
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrilAddItem()
+            }
+        })
+    }
     var ctrilAddItem = function() {
         //Get the field input data
         var input = UICtrl.getInput()
         console.log(input)
     }
-    document.querySelector('.add__btn').addEventListener('click', ctrilAddItem)
 
-    document.addEventListener('keypress', function(event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrilAddItem()
+    return {
+        init: function() {
+            console.log('Application has started.')
+            setupEventListeners()
         }
-    })
+    }
 })(budgetController, UIController)
+
+controller.init()
