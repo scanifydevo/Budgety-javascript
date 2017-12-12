3a382fa42ce37d37bfde18697eb7f87bee3c60c2
ivan-comar <guardian.sanddy@gmail.com>
2017-12-12 15:28:45 +0800

    update app file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Budget Controller
 var budgetController = (function() {
+
     var Expense = function(id, description, value) {
         this.id = id,
         this.description = description,
@@ -25,13 +26,33 @@ var budgetController = (function() {
 
     return {
         addItem: function(type, des, val) {
-
+            var newItem, ID
+            // create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1
+            } else {
+                ID = 0
+            }
+            // create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val)
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val)
+            }
+            // push it into our data structure
+            data.allItems[type].push(newItem)
+            // return the new element
+            return newItem
+        },
+        testing: function() {
+            console.log(data)
         }
     }
 })()
 
 // UI Controller
 var UIController = (function() {
+
     var DOMstrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
@@ -46,6 +67,21 @@ var UIController = (function() {
                 value: document.querySelector(DOMstrings.inputValue).value
             }
         },
+        addListItem: function(obj, type) {
+            var html, newHtml
+
+            // create HTML string iwth placeholder text
+            if (type === 'inc') {
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            } else if (type === 'exp') {
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+
+            // Replace the placeholder text with some actural data.
+            newHtml = html.replace('%id%', obj.id)
+            newHtml = newHtml.replace('%description%', obj.description)
+            newHtml = newHtml.replace('%value%', obj.value)
+        },
         getDOMstrings: function() {
             return DOMstrings
         }
@@ -54,21 +90,24 @@ var UIController = (function() {
 
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
+    
     var setupEventListeners = function() {
         var DOM = UICtrl.getDOMstrings()
         // click the add item
-        document.querySelector(DOM.inputBtn).addEventListener('click', ctrilAddItem)
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
         // press the enter 
         document.addEventListener('keypress', function(event) {
             if (event.keyCode === 13 || event.which === 13) {
-                ctrilAddItem()
+                ctrlAddItem()
             }
         })
     }
-    var ctrilAddItem = function() {
+    var ctrlAddItem = function() {
+        var input, newItem
         //Get the field input data
-        var input = UICtrl.getInput()
-        console.log(input)
+        input = UICtrl.getInput()
+        // add the item to the budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value)
     }
 
     return {
