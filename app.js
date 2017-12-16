8cd0256cdbdb46672a890277afbca86631f35ffc
ivan-comar <guardian.sanddy@gmail.com>
2017-12-16 18:42:25 +0800

    finishing

--- app.js
+++ app.js
@@ -140,7 +140,8 @@ var UIController = (function() {
         expensesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLable: '.budget__title--month'
     }
 
     var formatNumber = function(num, type) {
@@ -158,6 +159,12 @@ var UIController = (function() {
         dec = numSplit[1]
 
         return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec
+    }
+
+    var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i)
+        }
     }
 
     return {
@@ -208,9 +215,11 @@ var UIController = (function() {
             fieldsArr[0].focus()
         },
         displayBudget: function(obj) {
-            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget
-            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc
-            document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp
+            var type
+            obj.budget > 0 ? type = 'inc' : type = 'exp'
+            document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type)
+            document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc')
+            document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExp, 'exp')
             if (obj.percentage > 0) {
                 document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%'
             } else {
@@ -222,19 +231,37 @@ var UIController = (function() {
             
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel)
 
-            var nodeListForEach = function(list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i)
-                }
-            }
-
             nodeListForEach(fields, function(current, index) {
-                if (percentages[index] > 0) {
+                if (percentages[index] > 0) { 
                     current.textContent = percentages[index] + '%'
                 } else {
                     current.textContent = '---'
                 }
             })
+        },
+
+        displayMonth: function() {
+            var now, month, months, year
+            now = new Date()
+
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+            month = now.getMonth()
+            year = now.getFullYear()
+            document.querySelector(DOMstrings.dateLable).textContent = months[month] + ' ' + year
+        },
+
+        changedType: function() {
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' + 
+                DOMstrings.inputDescription + ',' +
+                DOMstrings.inputValue
+            )
+
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus')
+            })
+
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red')
         },
         
         getDOMstrings: function() {
@@ -258,6 +285,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         })
         
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem)
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType)
     }
 
     var updateBudget = function() {
@@ -329,6 +358,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     return {
         init: function() {
             console.log('Application has started.')
+            UICtrl.displayMonth()
             UICtrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
